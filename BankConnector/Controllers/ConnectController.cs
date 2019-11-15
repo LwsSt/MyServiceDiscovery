9bab6e0905584fc07c3d93d54fa114993fa81580
Lewis <lewis.stapleton@outlook.com>
2019-11-15 22:19:42 +0000

    Add a connector

---
+++ BankConnector/Controllers/ConnectController.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankConnector.Controllers
+{
+    [Route("[controller]")]
+    public class ConnectController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<string> Connect(ServiceRequest request)
+        {
+            return "Banking";
+        }
+    }
+}
