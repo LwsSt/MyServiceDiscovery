880f374ec40722ad22adce31fb2378ef2babc8b7
Lewis <lewis.stapleton@outlook.com>
2019-11-23 21:45:48 +0000

    Add ConnectorApiClient to Gateway

---
+++ Gateway/Client/ConnectorApiClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Common.Client
+{
+    public class ConnectorApiClient
+    {
+        private readonly HttpClient client;
+
+        public ConnectorApiClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<ServiceResponse> Connect(ServiceRequest request)
+        {
+            var response = await client.PostAsJsonAsync("connect", request);
+
+            return await response.Content.ReadAsAsync<ServiceResponse>();
+        }
+    }
+}
