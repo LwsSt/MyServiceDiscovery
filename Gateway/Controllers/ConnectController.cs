bd3fcdb7b5f6bc8ffb495599179e9b0120003801
Lewis <lewis.stapleton@outlook.com>
2019-11-27 20:37:47 +0000

    Call API Client from gateway

--- Gateway/Controllers/ConnectController.cs
+++ Gateway/Controllers/ConnectController.cs
@@ -1,15 +1,25 @@
 ﻿using Common;
+using Common.Client;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Gateway.Controllers
 {
     [Route("[controller]")]
     public class ConnectController : ControllerBase
     {
-        [HttpPost]
-        public ActionResult<ServiceResponse> Connect(ServiceRequest request)
+        private readonly ConnectorApiClient client;
+
+        public ConnectController(ConnectorApiClient client)
         {
-            return NoContent();
+            this.client = client;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse>> Connect(ServiceRequest request)
+        {
+            var response = await client.Connect(request);
+            return response;
         }
     }
 }
