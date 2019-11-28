a4697b3b9ff2f382c93ad15d74677e209bca5045
Lewis <lewis.stapleton@outlook.com>
2019-11-28 21:11:28 +0000

    Create Channel enum

--- Common/ServiceRequest.cs
+++ Common/ServiceRequest.cs
@@ -4,7 +4,7 @@ namespace Common
 {
     public class ServiceRequest
     {
-        public string Channel { get; set; }
+        public Channel Channel { get; set; }
         public ICollection<string> Scopes { get; set; }
     }
 }
