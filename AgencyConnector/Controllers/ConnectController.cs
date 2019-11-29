3d533aa1b98fe157306675af71ca524c56902d7d
Lewis <lewis.stapleton@outlook.com>
2019-11-29 20:05:11 +0000

    Add Agency Connector

---
+++ AgencyConnector/Controllers/ConnectController.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgencyConnector.Controllers
+{
+    [Route("[controller]")]
+    public class ConnectController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<ServiceResponse> Connect(ServiceRequest request)
+        {
+            return new ServiceResponse()
+            {
+                Connector = "Agency",
+                Response = string.Join(' ', request.Scopes)
+            };
+        }
+    }
+}
